605529bf464a2897f0d3866af244915d86acb626
diky <diky@diky-PC>
2020-11-06 22:59:24 +0700

    Upload Project

---
+++ src/WarnaOO.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * Nama  : Diva Sabila Ramadhan
+ * Kelas : IF1
+ * NIM   : 10119039
+ * Deskripsi Program : menampilkan warna kepribadian berdasarkan objek
+ */
+public class WarnaOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Warna wrn = new Warna();
+        wrn.inputWarna();
+        wrn.inputNama();
+        wrn.cekWarna();
+    }
+    
+}
